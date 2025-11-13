3e3a38816dc2731402595ae9749a63c3aa25eddc
nieee <anie_guo@dlsu.edu.ph>
2025-11-13 23:35:05 +0800

    Add files via upload

---
+++ src/Purchase.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+public class Purchase {
+    private int purchase_no;
+    private LocalDate purchase_date;
+    private int customer_id;
+
+    public Purchase(int no, LocalDate purchase_date, int cId)
+    {
+        purchase_no=no;
+        this.purchase_date=purchase_date;
+        customer_id=cId;
+    }
+    public int getPurchaseNo() { return purchase_no; }
+    public int getCustomerId() { return customer_id; }
+    public LocalDate getPurchaseDate() { return purchase_date; }
+}
