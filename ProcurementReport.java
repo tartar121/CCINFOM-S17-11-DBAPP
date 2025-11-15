542be99cb41928cb0521d7c3f6cffa3fd7754df6
andreajadyntan <andrea_jadyn_tan@dlsu.edu.ph>
2025-11-15 22:32:38 +0800

    fixed delivery and return

---
+++ ProcurementReport.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class ProcurementReport {
+    private int supplier_id;
+    private String supplier_name;
+    private int no_of_restocks;
+    private double total_amount;
+    public ProcurementReport(int supid, String supname, int norestocks, double total) {
+        supplier_id=supid;
+        supplier_name=supname;
+        no_of_restocks=norestocks;
+        total_amount=total;
+    }
+    public int getSID()
+    {
+        return supplier_id;
+    }
+    public String getSName()
+    {
+        return supplier_name;
+    }
+    public int getRestocks()
+    {
+        return no_of_restocks;
+    }
+    public double getTotal()
+    {
+        return total_amount;
+    }
+}
---
+++ ReturnableItem.java
@@ -0,0 +1,37 @@
+package Model;
+
+import java.time.LocalDate;
+
+// Helper class to store results of query
+// NOT a database table
+public class ReturnableItem {
+    private int medicineId; // Also the Batch ID
+    private String medicineName;
+    private int quantity;
+    private LocalDate expirationDate;
+    private int deliveryNo;
+    private LocalDate shippedDate;
+    private double priceBought;
+
+    public ReturnableItem(int medicineId, String medName, int qty,
+                          LocalDate expDate, int delNo,
+                          LocalDate shipDate, double priceBought) {
+        this.medicineId = medicineId;
+        this.medicineName = medName;
+        this.quantity = qty;
+        this.expirationDate = expDate;
+        this.deliveryNo = delNo;
+        this.shippedDate = shipDate;
+        this.priceBought = priceBought;
+    }
+
+    // Getters
+    public int getMedicineId() { return medicineId; }
+    public String getMedicineName() { return medicineName; }
+    public int getQuantity() { return quantity; }
+    public LocalDate getExpirationDate() { return expirationDate; }
+    public int getDeliveryNo() { return deliveryNo; }
+    public LocalDate getShippedDate() { return shippedDate; }
+    public double getPriceBought() { return priceBought; }
+
+}
