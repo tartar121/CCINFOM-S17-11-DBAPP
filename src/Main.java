c2231c2def2c1cb18c2663deb21af2c2f36bc197
Tara Ysabel P. Uy <tara_uy@dlsu.edu.ph>
2025-11-15 09:30:55 +0800

    refactor: add import MainView from View and comment

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+// Do Ctrl + Shift + P to clean up this file if needed
+// So that the packages will load properly in your IDE
+import View.MainView;
+
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
