443f0d761dfae1ba0f62e1b03e2248228632392e
andreajadyntan <andrea_jadyn_tan@dlsu.edu.ph>
2025-11-18 13:27:14 +0800

    removed some variables

--- src/Model/NewDeliveryItem.java
+++ src/Model/NewDeliveryItem.java
@@ -5,28 +5,30 @@ import java.time.LocalDate;
 // This is the "Shopping List" 📝 helper class
 // It holds the data from the JTable before it's processed
 public class NewDeliveryItem {
-    private int medicineId;
-    private String name;
+    private int medicineId; // This is the Batch ID
+    private String medicineName;
+    private int quantityOrdered;
     private double priceBought;
     private double priceForSale;
-    private int quantity;
     private LocalDate expDate;
+    private double lineTotal;
 
-    public NewDeliveryItem(int id, String name, double pBought, 
-                           double pSale, int qty, LocalDate date) {
-        this.medicineId = id;
-        this.name = name;
-        this.priceBought = pBought;
-        this.priceForSale = pSale;
-        this.quantity = qty;
-        this.expDate = date;
+    public NewDeliveryItem(Medicine medicine, int quantity) {
+        this.medicineId = medicine.getId();
+        this.medicineName = medicine.getName();
+        this.priceBought = medicine.getPriceBought();
+        this.priceForSale = medicine.getPriceForSale();
+        this.expDate = medicine.getExpirationDate();
+        this.quantityOrdered = quantity;
+        lineTotal=quantityOrdered*priceBought;
     }
 
     // Getters
     public int getMedicineId() { return medicineId; }
-    public String getName() { return name; }
+    public String getName() { return medicineName; }
     public double getPriceBought() { return priceBought; }
     public double getPriceForSale() { return priceForSale; }
-    public int getQuantity() { return quantity; }
+    public int getQuantity() { return quantityOrdered; }
     public LocalDate getExpDate() { return expDate; }
+    public double getTotal() { return lineTotal; }
 }
